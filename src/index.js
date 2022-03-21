459291c340f310b5ad051b932c1fb2434d17e66c
jae <jae@pop-os.localdomain>
2022-03-21 23:02:52 +0100

    adding comparison

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+import { createReadStream, fstat, readFileSync, writeFileSync } from 'fs';
 import express from "express";
+import Jimp from 'jimp';
 
 const app = express();
 
 const PORT = 1337;
 
-app.use(express.static('./public'))
+app.use(express.json());
 
+app.use(express.static('./public'));
+
+Jimp.read(readFileSync('./public/img/6.png'));
+
+app.post('/submit', async (req, res) => {
+    const { data } = req.body;
+    
+    const original = await Jimp.read(readFileSync('./public/img/6.png'));
+    const value = await Jimp.read(Buffer.from(data.split(',')[1], 'base64'));
+
+    let DIFF = 0;
+
+    for(let i = 0; i < original.bitmap.data.length; i+=1){
+        DIFF += Math.abs(original.bitmap.data[i] - value.bitmap.data[i]);
+    }
+
+    const percentage = 100 - (DIFF / original.bitmap.data.length)
+
+
+    //#51B5A9
+
+    res.json({
+        success: true,
+        percentage: percentage
+    });
+});
 
 app.listen(PORT, function(){
     console.log(`Listening on port ${PORT}`);
 });
+
