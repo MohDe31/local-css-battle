b6630f3754d76a645abd9ba309752d82031937bf
Rami Guessab <ramiguessab94@gmail.com>
2023-04-15 04:39:00 +0100

    fixed bug

--- src/index.js
+++ src/index.js
@@ -16,54 +16,53 @@ app.use(express.json());
 
 app.use(express.static("./public"));
 
-io.on("connection", (socket) => {
-  const users_submitted = [];
+io.on("connection", (socket) => {});
+const users_submitted = [];
 
-  app.get("/challenge", (req, res) => {
-    res.json({
-      data: `data:image/png;base64,${CURRENT_CHALLENGE}`,
-    });
+app.get("/challenge", (req, res) => {
+  res.json({
+    data: `data:image/png;base64,${CURRENT_CHALLENGE}`,
   });
+});
 
-  app.get("/submited", (req, res) => {
-    return res.json({ users_submitted });
-  });
+app.get("/submited", (req, res) => {
+  return res.json({ users_submitted });
+});
 
-  app.post("/submit", async (req, res) => {
-    const { data, uname } = req.body;
+app.post("/submit", async (req, res) => {
+  const { data, uname } = req.body;
 
-    if (users_submitted.some((user) => user.uname === uname)) {
-      return res.json({
-        success: false,
-        reason: "Already submitted please wait for the result",
-      });
-    }
-
-    const original = await Jimp.read(Buffer.from(CURRENT_CHALLENGE, "base64"));
-    const value = await Jimp.read(Buffer.from(data.split(",")[1], "base64"));
-
-    let DIFF = 0;
-
-    for (let i = 0; i < original.bitmap.data.length; i += 1) {
-      const e = Math.abs(original.bitmap.data[i] - value.bitmap.data[i]);
-      DIFF += e < 1 ? 0 : 255;
-    }
-
-    const error =
-      (100 * (DIFF / (original.bitmap.data.length - 400 * 300))) / 255;
-
-    const percentage = 100 - error;
-
-    users_submitted.push({ uname, percentage });
-
-    socket.broadcast.emit("submit", { uname, percentage });
-
-    //#51B5A9
-
-    res.json({
-      success: true,
-      percentage: percentage,
+  if (users_submitted.some((user) => user.uname === uname)) {
+    return res.json({
+      success: false,
+      reason: "Already submitted please wait for the result",
     });
+  }
+
+  const original = await Jimp.read(Buffer.from(CURRENT_CHALLENGE, "base64"));
+  const value = await Jimp.read(Buffer.from(data.split(",")[1], "base64"));
+
+  let DIFF = 0;
+
+  for (let i = 0; i < original.bitmap.data.length; i += 1) {
+    const e = Math.abs(original.bitmap.data[i] - value.bitmap.data[i]);
+    DIFF += e < 1 ? 0 : 255;
+  }
+
+  const error =
+    (100 * (DIFF / (original.bitmap.data.length - 400 * 300))) / 255;
+
+  const percentage = 100 - error;
+
+  users_submitted.push({ uname, percentage });
+
+  io.sockets.emit("submit", { uname, percentage });
+
+  //#51B5A9
+
+  res.json({
+    success: true,
+    percentage: percentage,
   });
 });
 
