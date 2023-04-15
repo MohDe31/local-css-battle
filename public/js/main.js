b06df9ad43f0e442d1613fc68a1de08193046259
Rami Guessab <ramiguessab94@gmail.com>
2023-04-15 03:38:34 +0100

    added score-board

--- public/js/main.js
+++ public/js/main.js
@@ -4,22 +4,20 @@ let editor;
 let uname;
 let prevDoc;
 /**
- * 
- * @param {MouseEvent} e 
+ *
+ * @param {MouseEvent} e
  */
+
 function onMouseMove(e) {
-    const { x, y, target } = e;
+  const { x, y, target } = e;
 
-    const { top, left } = target.getBoundingClientRect()
+  const { top, left } = target.getBoundingClientRect();
 
+  const xPos = x - left;
+  const yPos = y - top;
 
-    const xPos = x - left;
-    const yPos = y - top;
-
-
-    MX = xPos;
-    MY = yPos;
-
+  MX = xPos;
+  MY = yPos;
 }
 
 const image1 = new MarvinImage();
@@ -31,115 +29,123 @@ let im2 = false;
 let image_data;
 
 async function sStyle() {
-    // const tex = document.querySelector('#editor');
-    prevDoc.body.innerHTML = editor.getValue();
+  // const tex = document.querySelector('#editor');
+  prevDoc.body.innerHTML = editor.getValue();
 
-    const canv = await html2canvas(prevDoc.body);
-    const b64 = canv.toDataURL();
+  const canv = await html2canvas(prevDoc.body);
+  const b64 = canv.toDataURL();
 
-    image1.load(b64);
+  image1.load(b64);
 }
 
-async function submit(){
-
-    const canv = await html2canvas(prevDoc.body);
-    const b64 = canv.toDataURL();
-    image1.load(b64, async function () {
-
-        if(!uname){
-            alert('SOMETHING WENT WRONG');
-        }
-        const response = await fetch('/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                uname: uname,
-                data: b64
-            })
-        });
-    
-        if(response.status !== 200)
-        {
-          alert('something went wrong please report to the admin!');
-          location.reload();
-        }
-        const json = await response.json();
-
-        if(json.success)
-        {
-          startSplashScreen(parseFloat(json.percentage));
-        }
-
-        // alert(`You got ${Math.round(json.percentage)}%!`);            
+async function submit() {
+  const canv = await html2canvas(prevDoc.body);
+  const b64 = canv.toDataURL();
+  image1.load(b64, async function () {
+    if (!uname) {
+      alert("SOMETHING WENT WRONG");
+    }
+    const response = await fetch("/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        uname: uname,
+        data: b64,
+      }),
     });
+
+    if (response.status !== 200) {
+      alert("something went wrong please report to the admin!");
+      location.reload();
+    }
+    const json = await response.json();
+
+    if (json.success) {
+      startSplashScreen(parseFloat(json.percentage));
+    }
+
+    // alert(`You got ${Math.round(json.percentage)}%!`);
+  });
 }
 
 const diff = true;
 
-
 function startSplashScreen(percentage) {
   const splash = document.querySelector(".splash");
-  const perc   = document.querySelector("#perc");
+  const perc = document.querySelector("#perc");
   const perc_ctn = document.querySelector("#perc-ctn");
-  perc.innerHTML = percentage.toFixed(2) + '%';
-  splash.classList.toggle('show');
-  perc_ctn.setAttribute('style', `clip: rect(0px, calc(var(--w) * ${percentage/100}), var(--h), 0px)`);
+  perc.innerHTML = percentage.toFixed(2) + "%";
+  splash.classList.toggle("show");
+  perc_ctn.setAttribute(
+    "style",
+    `clip: rect(0px, calc(var(--w) * ${percentage / 100}), var(--h), 0px)`
+  );
 }
 
 /**
- * 
- * @param {CanvasRenderingContext2D} ctx 
+ *
+ * @param {CanvasRenderingContext2D} ctx
  */
 function render(ctx) {
-    if (im1 && im2) {
+  if (im1 && im2) {
+    for (let i = 0; i < image1.getWidth(); i++) {
+      for (let j = 0; j < image1.getHeight(); j++) {
+        // image_data.data[(j * 400 + i) * 4] = image1.getIntComponent0(i, j);
+        // image_data.data[(j * 400 + i) * 4 + 1] = image1.getIntComponent1(i, j);
+        // image_data.data[(j * 400 + i) * 4 + 2] = image1.getIntComponent2(i, j);
 
-        for (let i = 0; i < image1.getWidth(); i++) {
-            for (let j = 0; j < image1.getHeight(); j++) {
+        if (i <= MX) {
+          const R = Math.abs(
+            image1.getIntComponent0(i, j) -
+              (diff ? image2.getIntComponent0(i, j) : 0)
+          );
+          const G = Math.abs(
+            image1.getIntComponent1(i, j) -
+              (diff ? image2.getIntComponent1(i, j) : 0)
+          );
+          const B = Math.abs(
+            image1.getIntComponent2(i, j) -
+              (diff ? image2.getIntComponent2(i, j) : 0)
+          );
 
-                // image_data.data[(j * 400 + i) * 4] = image1.getIntComponent0(i, j);
-                // image_data.data[(j * 400 + i) * 4 + 1] = image1.getIntComponent1(i, j);
-                // image_data.data[(j * 400 + i) * 4 + 2] = image1.getIntComponent2(i, j);
-                
-                if (i <= MX) {
-
-                    const R = Math.abs(image1.getIntComponent0(i, j) - (diff ? image2.getIntComponent0(i, j) : 0));
-                    const G = Math.abs(image1.getIntComponent1(i, j) - (diff ? image2.getIntComponent1(i, j) : 0));
-                    const B = Math.abs(image1.getIntComponent2(i, j) - (diff ? image2.getIntComponent2(i, j) : 0));
-
-                    image_data.data[(j * 400 + i) * 4] = R;
-                    image_data.data[(j * 400 + i) * 4 + 1] = G;
-                    image_data.data[(j * 400 + i) * 4 + 2] = B;
-                } else {
-                    image_data.data[(j * 400 + i) * 4] = image2.getIntComponent0(i, j);
-                    image_data.data[(j * 400 + i) * 4 + 1] = image2.getIntComponent1(i, j);
-                    image_data.data[(j * 400 + i) * 4 + 2] = image2.getIntComponent2(i, j);
-                }
-
-
-                
-               image_data.data[(j * 400 + i) * 4 + 3] = 255;
-            }
+          image_data.data[(j * 400 + i) * 4] = R;
+          image_data.data[(j * 400 + i) * 4 + 1] = G;
+          image_data.data[(j * 400 + i) * 4 + 2] = B;
+        } else {
+          image_data.data[(j * 400 + i) * 4] = image2.getIntComponent0(i, j);
+          image_data.data[(j * 400 + i) * 4 + 1] = image2.getIntComponent1(
+            i,
+            j
+          );
+          image_data.data[(j * 400 + i) * 4 + 2] = image2.getIntComponent2(
+            i,
+            j
+          );
         }
 
-        ctx.putImageData(image_data, 0, 0);
+        image_data.data[(j * 400 + i) * 4 + 3] = 255;
+      }
     }
 
-    requestAnimationFrame(function () {
-        render(ctx);
-    });
+    ctx.putImageData(image_data, 0, 0);
+  }
+
+  requestAnimationFrame(function () {
+    render(ctx);
+  });
 }
 
 window.onload = async function () {
-    prevDoc = document.getElementById('preview').contentWindow.document;
-    prevDoc.body.style.height = '300px';
-    prevDoc.body.style.width  = '400px';
-    prevDoc.body.style.background  = 'white';
-    prevDoc.body.style.overflow = 'hidden';
+  prevDoc = document.getElementById("preview").contentWindow.document;
+  prevDoc.body.style.height = "300px";
+  prevDoc.body.style.width = "400px";
+  prevDoc.body.style.background = "white";
+  prevDoc.body.style.overflow = "hidden";
 
-    const tex = document.querySelector('#editor');
-    tex.value = `
+  const tex = document.querySelector("#editor");
+  tex.value = `
 <div class="ctn"></div>
 <style>
 * {
@@ -154,67 +160,58 @@ window.onload = async function () {
 </style>
     `;
 
-    editor = CodeMirror.fromTextArea(tex, {
-        lineNumbers: true,
-        mode: 'htmlmixed',
-        theme: 'dracula',
-        width: '100%',
-        height: '100%'
-    });
+  editor = CodeMirror.fromTextArea(tex, {
+    lineNumbers: true,
+    mode: "htmlmixed",
+    theme: "dracula",
+    width: "100%",
+    height: "100%",
+  });
 
+  editor.on("keyup", sStyle);
+  // editor.setSize(400, 500);
 
-    editor.on('keyup', sStyle);
-    // editor.setSize(400, 500);
+  prevDoc.body.innerHTML = tex.value;
 
-    prevDoc.body.innerHTML = tex.value;
+  const response = await fetch("/challenge");
+  const json = await response.json();
 
-    const response = await fetch('/challenge');
-    const json     = await response.json();
+  IMAGE_1 = json.data;
 
-    IMAGE_1 = json.data;
+  const canv = await html2canvas(prevDoc.body);
+  const b64 = canv.toDataURL();
 
-    const canv = await html2canvas(prevDoc.body);
-    const b64 = canv.toDataURL();
+  image1.load(b64, function () {
+    im1 = true;
+  });
 
+  image2.load(IMAGE_1, function () {
+    im2 = true;
+  });
 
-    image1.load(b64, function () {
-        im1 = true;
-    });
+  const canvas = document.querySelector("#display");
 
-    image2.load(IMAGE_1, function () {
-        im2 = true;
-    });
+  canvas.onmousemove = onMouseMove;
+  canvas.onmouseout = function () {
+    MX = -1;
+  };
 
+  /** @type {CanvasRenderingContext2D} */
+  const ctx = canvas.getContext("2d");
 
+  image_data = ctx.createImageData(400, 300);
 
+  ctx.putImageData(image_data, 0, 0);
 
-    const canvas = document.querySelector('#display');
+  uname = localStorage.getItem("uname");
 
-    canvas.onmousemove = onMouseMove;
-    canvas.onmouseout = function(){
-        MX = -1;
-    }
+  if (!uname) {
+    uname = prompt("Please enter a username").trim().split(" ").join("_");
 
-    /** @type {CanvasRenderingContext2D} */
-    const ctx = canvas.getContext('2d');
+    localStorage.setItem("uname", uname);
+  }
 
-    image_data = ctx.createImageData(400, 300);
-
-    ctx.putImageData(image_data, 0, 0);
-
-
-
-    uname = localStorage.getItem('uname');
-
-    if (!uname){
-        uname = prompt('Please enter a username').trim().split(' ').join('_');
-
-        localStorage.setItem('uname', uname);
-    }
-
-    requestAnimationFrame(function () {
-        render(ctx);
-    });
-
-}
-
+  requestAnimationFrame(function () {
+    render(ctx);
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,61 +1,72 @@
 import express from "express";
-import Jimp from 'jimp';
-import { CURRENT_CHALLENGE } from './image.js';
+import { createServer } from "http";
+import { Server } from "socket.io";
+import Jimp from "jimp";
+import { CURRENT_CHALLENGE } from "./image.js";
 
 const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server);
 
 const PORT = 1337;
 
 app.use(express.json());
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-const users_submitted = [];
+io.on("connection", (socket) => {
+  const users_submitted = [];
 
-app.get('/challenge', (req, res) => {
+  app.get("/challenge", (req, res) => {
     res.json({
-        data: `data:image/png;base64,${CURRENT_CHALLENGE}`
-    })
-});
+      data: `data:image/png;base64,${CURRENT_CHALLENGE}`,
+    });
+  });
 
-app.post('/submit', async (req, res) => {
+  app.get("/submited", (req, res) => {
+    return res.json({ users_submitted });
+  });
+
+  app.post("/submit", async (req, res) => {
     const { data, uname } = req.body;
 
-    if(users_submitted.includes(uname)) {
-        return res.json({
-            success: false,
-            reason: 'Already submitted please wait for the result'
-        });
+    if (users_submitted.some((user) => user.uname === uname)) {
+      return res.json({
+        success: false,
+        reason: "Already submitted please wait for the result",
+      });
     }
-    
-    const original = await Jimp.read(Buffer.from(CURRENT_CHALLENGE, 'base64'));
-    const value = await Jimp.read(Buffer.from(data.split(',')[1], 'base64'));
+
+    const original = await Jimp.read(Buffer.from(CURRENT_CHALLENGE, "base64"));
+    const value = await Jimp.read(Buffer.from(data.split(",")[1], "base64"));
 
     let DIFF = 0;
 
-    for(let i = 0; i < original.bitmap.data.length; i+=1){
-        const e = Math.abs(original.bitmap.data[i] - value.bitmap.data[i]);
-        DIFF += e < 1? 0: 255;
-        
+    for (let i = 0; i < original.bitmap.data.length; i += 1) {
+      const e = Math.abs(original.bitmap.data[i] - value.bitmap.data[i]);
+      DIFF += e < 1 ? 0 : 255;
     }
 
-    const error = 100 * (DIFF / (original.bitmap.data.length - (400*300))) / 255;
+    const error =
+      (100 * (DIFF / (original.bitmap.data.length - 400 * 300))) / 255;
 
     const percentage = 100 - error;
 
-    users_submitted.push(uname);
+    users_submitted.push({ uname, percentage });
 
-    console.log(uname, percentage);
+    socket.broadcast.emit("submit", { uname, percentage });
 
     //#51B5A9
 
     res.json({
-        success: true,
-        percentage: percentage
+      success: true,
+      percentage: percentage,
     });
+  });
 });
 
-app.listen(PORT, function(){
-    console.log(`Listening on port ${PORT}`);
+server.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
 });
-
